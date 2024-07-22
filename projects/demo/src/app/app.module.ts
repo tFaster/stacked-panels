9b2a9db2bb6af7114966c855887b876bf9698b89
Timo Schneller <t.schneller@gmail.com>
2024-07-22 16:17:34 +0200

    build(deps): Update Angular to v18.1.1 (#66)
    * build(deps): npm audit fix
    
    * build(deps): update Angular to v18.1.1
    
    * build(deps): update 3rd party dependencies

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { StackedPanelsModule } from '@tfaster/stacked-panels';
 import { AppComponent } from './app.component';
 
@@ -9,14 +9,17 @@ import { AppComponent } from './app.component';
   declarations: [
     AppComponent
   ],
+  bootstrap: [
+    AppComponent
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    HttpClientModule,
     StackedPanelsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    provideHttpClient(withInterceptorsFromDi())
+  ]
 })
 export class AppModule {
 }
